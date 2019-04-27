5c77591847c93e845f93aff428d93af398832347
romkravets <romann.kravets@gmail.com>
2019-04-27 11:41:12 +0300

    Added dynemic accordion

--- src/index/index.js
+++ src/index/index.js
@@ -3,4 +3,30 @@ import './styles/accordion.scss';
 
 import { accordion} from './scripts/accordion';
 
-accordion(document.querySelector('#simpleAccordion'));
+const accordionSection = [
+   {
+      title: 'Section 1',
+      content: `<ul>
+                  <li>Test 1</li>
+                  <li>Test 2</li>
+                  <li>Test 3</li>
+                  <li>Test 4</li>
+                  <li>Test 5</li>
+                  <li>Test 6</li>
+                  <li>Test 7</li>
+               </ul>
+      `
+   },
+   {
+      title: 'Section 2',
+      content:`Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Tempore animi omnis debitis ad inventore accusantium vero itaque eligendi, 
+      cumque ullam quibusdam aut, laboriosam dolore culpa similique quae dolor alias et?`
+   },
+   {
+      title: 'Section 3',
+      content:`Lorem ipsum dolor sit amet consectetur adipisicing elit. `
+   },
+];
+
+accordion(document.querySelector('#simpleAccordion'), accordionSection);
--- src/index/scripts/accordion.js
+++ src/index/scripts/accordion.js
@@ -1,9 +1,37 @@
-const TITLE_CLASS_NAME = 'accordion__title';
-const EXPAND_CLASS_NAME = 'accordion__panel_expand';
+const ROOT_CLASS_NAMES = 'accordion';
+const TITLE_CLASS_NAME =  `${ROOT_CLASS_NAMES}__title`;
+const PANEL_CLASS_NAME = `${ROOT_CLASS_NAMES}__panel`;
+const CONTENT_CLASS_NAME = `${ROOT_CLASS_NAMES}__content`;
+const EXPAND_CLASS_NAME = `${PANEL_CLASS_NAME}_expand`;
 
-export function accordion(targetElement) {
-   const controls = targetElement.querySelectorAll(`.${TITLE_CLASS_NAME}`);
+export function accordion(targetElement, accordionSections) {
+   let controls;
    let expandedElement;
+
+   function render() {
+      targetElement.classList.add(ROOT_CLASS_NAMES);
+      renderPanels()
+   }
+
+   function renderPanels() {
+      for (let section of accordionSections) {
+         let panel = document.createElement('div');
+         let control = document.createElement('button');
+         let content = document.createElement('div');
+
+         control.classList.add(TITLE_CLASS_NAME);
+         panel.classList.add(PANEL_CLASS_NAME);
+         content.classList.add(CONTENT_CLASS_NAME);
+
+         control.textContent = section.title;
+         content.innerHTML = section.content;
+
+         control.addEventListener('click', toggle);
+         panel.appendChild(control);
+         panel.appendChild(content);
+         targetElement.appendChild(panel);
+      }
+   }
 
    function expand(panel) {
       if (expandedElement) {
@@ -29,11 +57,6 @@ export function accordion(targetElement) {
       }
    }
 
-   function handleEvents() {
-      for(const control of controls) {
-         control.addEventListener('click', toggle);
-      }
-   }
 
-   handleEvents();
+   render();
 }
